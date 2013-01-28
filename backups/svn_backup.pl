0b34d5d7914254b7b313a737571b03d6795bfd77
Stan <stan+git@stanandliz.net>
2013-01-28 01:16:36 +0000

    Hacked massively to correct call to our var conf file and also to include region call in order to avoid error being thrown. Added stages required for backup process.

--- backups/svn_backup.pl
+++ backups/svn_backup.pl
@@ -12,15 +12,22 @@ use LWP::Simple;
 # Only thing to note is that nothing is encrypted so access to
 # source is only as secure as EC2's access policies.
 
+my $svn_mount = "/opt/svn";
+
 # We source our EC2 details from another file as I don't want
 # my Amazon credentials available on Git
 # We set the path by making our location a lib:
 use lib  '/opt/aws';
-use aws_keys;
+use Aws_keys;
 # Our keys should be in a file like this:
 # filename "/opt/aws/aws_keys.pm"
 # ----------------------------------
+# package Aws_keys
+# use strict;
+# use Exporter 'import';
+# our @ISA = 'Exporter';
 # our ($accesskeyid, $secret );
+# our @EXPORT = qw($accesskeyid $secret);
 # $accesskeyid = "1234";
 # $secret = "4567";
 # ----------------------------------
@@ -29,10 +36,14 @@ use aws_keys;
 
 # Create a connection to AWS:
 
-#my $ec2 = Net::Amazon::EC2->new(
-#        AWSAccessKeyId => $accesskeyid, 
-#        SecretAccessKey => $secret
-#);
+print "sanity: $accesskeyid, $secret\n";
+
+my $ec2 = Net::Amazon::EC2->new(
+        AWSAccessKeyId =>  "$accesskeyid", 
+        SecretAccessKey => "$secret",
+	region => "eu-west-1"
+) || die "unable to make connection\n";
+
 
 # Get our instance ID using perl lwp module.
 # All EC2 instances cat get info about themselves
@@ -44,5 +55,21 @@ if (!defined $instance_id) {
     die "failed to fetch URL $url";
 }
 
-print "$instance_id\n";
+# Scan fstab for the SVN mount point as defined in our var
+# We want to confirm it's xfs and get the device info
+# so we can search for it whene we get our instance info
+
+# Let's get some info on the host and we can then confirm our
+# ebs volume we want to snapshot
+my $instance_info = $ec2->describe_instances(InstanceId => "$instance_id"); 
+
+# Our first backup task is to snapshot our xfs volume.
+# 1. We freeze the volume
+
+# 2. We snapshot the volume
+
+# 3. We mount the backup EBS volume
+# 4. We tar and gzip the svn repo into the EBS volume
+# 5. We unfreeze the volume 
+# We email admin with the results of the backup
 
